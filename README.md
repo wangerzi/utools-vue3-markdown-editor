ba2aefa85b1753f3b9c8324d2294c8de833231d6
Jeffrey-Wang <admin@wj2015.com>
2021-08-11 23:50:08 +0800

    feat: complete version 1.0.0.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # utools-vue3-markdown-editor
-A simple UTools plugin via vue3 + vite + elementui
+基于 vue3 + vite + elementui + markd + keyboardjs + highlight.js 实现的清爽 Markdown 编辑器
+
+![screen](./screen.gif)
+
+## 功能
+
+- 支持复制文件触发编辑
+- `Ctrl + S` 即时保存，`Ctrl + Shift + S` 另存为
+- 支持代码高亮，即时渲染 Github 样式的预览
+
+## 开源协议
+
+utools-vue3-markdown-editor is licensed under the MIT License. See [LICENSE](https://github.com/GitbookIO/gitbook/blob/master/LICENSE) for the full license text.
--- public/preload.js
+++ public/preload.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+
 console.log("preload js loaded")
+
+window.readMarkdownFile = function (path) {
+  if (path.match(/\.md$/i)) {
+    return fs.readFileSync(path, {
+      encoding: "utf-8"
+    });
+  } else {
+    return "";
+  }
+}
+
+window.writeMarkdownFile = function (path, content) {
+  if (fs.existsSync(path)) {
+    if (path.match(/\.md$/i)) {
+      fs.writeFileSync(path, content)
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    fs.writeFileSync(path, content)
+    return true;
+  }
+}
